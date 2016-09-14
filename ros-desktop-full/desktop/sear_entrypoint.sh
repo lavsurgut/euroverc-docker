d040824c26213175a93abca318f5db7435c175b5
Valery Lavrentiev <lavsurgut@gmail.com>
2016-09-14 16:02:00 +0200

    fixed usb permissions check to be performed once

--- ros-desktop-full/desktop/sear_entrypoint.sh
+++ ros-desktop-full/desktop/sear_entrypoint.sh
@@ -2,13 +2,13 @@
 set -e
 
 # workaround to set rights for usb devices
-# otherwise it is not possible to write as non-root
+# otherwise it is not possible to write into usb as non-root
 # in container
-if [ "$(whoami)" != 'root' ]; then
+USB_PERMS=$(stat /dev/bus/usb | sed -n '/^Access: (/{s/Access: (\([0-9]\+\).*$/\1/;p}')
+
+if [ "$(whoami)" != 'root' ] && [ "${USB_PERMS}" != '0777' ]; then
 	gosu root chmod -R 777 /dev/bus/usb
 fi
-
-#export ROS_IP=$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)
 
 exec /ros_entrypoint.sh "$@"
 
